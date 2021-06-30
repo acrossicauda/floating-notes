8b4e0992c30e2b47168421d541254aac73768a0f
Tiago <acrossicauda@hotmail.com.br>
2021-06-30 19:31:34 -0300

    adicionando saves e gets dos textos, mas precisa corrigir

--- scripts.js
+++ scripts.js
@@ -1,24 +1,40 @@
 function toogleFloatingNote() {
     $('#float-notes').toggle();
+    if(!$('#float-notes').is(':hidden')) {
+        $("#txtNotes").val(getNotes(1));
+    }
 }
 
 function getItens() {
     ;
 }
-function saveNotes(content) {
-    //alert('saveNotes');
+function saveNotes(content, idnote) {
     console.log(content);
+    //localStorage.setItem('note_1' + idnote, $content);
+    localStorage.setItem('note_' + idnote, $content);
+}
+
+function getNotes(idnote = 1) {
+    var notesValue = localStorage.getItem('note_' + idnote);
+    //var notesValue = localStorage.getItem('note_1');
+    return notesValue ? notesValue : '';
 }
 
 $(function() {
+    window.txtNoteClicked = false;
+    $('#txtNotes').click(function() {
+        window.txtNoteClicked = true;
+    });
     // so ira salvar quando clicar fora do textarea
-    $(document).on('mouseup', '*:not(.textarea)', function() {
+    $(':not(.textarea)').bind('click', function() {
+        window.txtNoteClicked = false;
         if(
-            ($('#float-notes').is(':hidden') === false)
-            && ($("#txtNotes").attr('id') != 'txtNotes')
+            $('#float-notes').is(':hidden') === false
+            && ($(this).attr('id') != 'float-notes')
         ) {
+
             try {
-                saveNotes($("#txtNotes").text());
+                saveNotes($("#txtNotes").val(), 1);
             } catch (ex) {}
         }
     });
